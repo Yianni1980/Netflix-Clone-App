4445d9ddc3f48eda4871955068832d9a909e8d31
Yianni1980 <ioagian1980@gmail.com>
2022-05-20 16:45:13 +0300

    Creating View Controller for TaB Bar

---
+++ Netflix Clone/Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by ioannis giannakidis on 20/5/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+
+    }
+    
+
+    
+
+}
---
+++ Netflix Clone/Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by ioannis giannakidis on 20/5/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+  
+
+}
