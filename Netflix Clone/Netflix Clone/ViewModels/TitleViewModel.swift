4aeffd7427d21e172fce4abb90bc17f5f00f0a2f
Yianni1980 <ioagian1980@gmail.com>
2022-06-06 02:20:26 +0300

    finished UpcomingViewController

---
+++ Netflix Clone/Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by ioannis giannakidis on 6/6/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName:String
+    let posterURL:String
+}
